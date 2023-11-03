a1c704b308883df7de43e0914615c9be9afdcf0d
John Rabe <146613450+Johnrabe@users.noreply.github.com>
2023-11-03 12:54:46 +0800

    Add files via upload

---
+++ aggregates_sum_ave_etc.sql
@@ -0,0 +1,8 @@
+SELECT 
+   #COUNT(salary) AS Salaries
+   #SUM(salary) AS Totals
+   #MIN() AS Lowest
+   #MAX() AS Highest
+   #AVG(salary) AS Average
+   ROUND(AVG(salary),2) AS Round
+FROM salaries
